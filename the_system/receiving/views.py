911a99d99cf19ed9a45ffb4abd6639cfcf42f1ed
yazin24 <yazinconiendo@gmail.com>
2023-11-04 11:36:58 +0800

    ....

--- the_system/receiving/views.py
+++ the_system/receiving/views.py
@@ -7,3 +7,19 @@ def main_page(request):
 def home_page(request):
     
     return render(request, 'home_receiving.html')
+
+def receiving_monitoring(request):
+    
+    return render(request, 'receiving_body.html')
+
+def received_po(request):
+    
+    return render(request, 'received_po.html')
+
+def items(request):
+    
+    return render(request, 'items.html')
+
+def undelivered(request):
+    
+    return render(request, 'undelivered.html')
