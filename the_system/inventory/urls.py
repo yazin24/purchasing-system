2c5a0533beabd4b00f9a21ae4a2756fe40fdabff
yazin24 <yazinconiendo@gmail.com>
2023-11-04 12:04:35 +0800

    ....

--- the_system/inventory/urls.py
+++ the_system/inventory/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name = 'mainpage'),
+    path('home/', views.home_page, name = 'homepage'),
+    path('items-monitoring/', views.items_monitoring, name = 'itemsmonitoring'),
+    path('suppliers/', views.suppliers, name = 'suppliers'),
+    path('receiving-history/', views.receiving_history, name = 'receivinghistory'),
 ]
