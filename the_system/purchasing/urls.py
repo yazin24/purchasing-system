cf63fcac2d5bb4fd0e3cf4ccf812a62d81821549
yazin24 <yazinconiendo@gmail.com>
2023-11-17 17:28:22 +0800

    .....

--- the_system/purchasing/urls.py
+++ the_system/purchasing/urls.py
@@ -3,10 +3,16 @@ from . import views
 
 
 urlpatterns = [
+    #url for navbar
+    
     path('', views.main_page, name = 'mainpage'),
     path('home/', views.purchasing_home_page, name = 'purchasinghomepage'),
     path('add-po/', views.purchasing_add_po, name = 'purchasingaddpo'),
     path('purchase-monitoring', views.purchasing_purchase_monitoring, name = 'purchasingpurchasemonitoring'),
     path('suppliers/', views.purchasing_suppliers, name = 'purchasingsuppliers'),
     path('products/', views.purchasing_products, name = 'purchasingproducts'),
+    
+    # url for functions
+    
+    
 ]
