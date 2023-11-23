655b8011e73dde9f5472deb459e4dd3f36c125d4
yazin24 <yazinconiendo@gmail.com>
2023-11-23 12:25:13 +0800

    ......

--- the_system/purchasing/forms.py
+++ the_system/purchasing/forms.py
@@ -5,9 +5,9 @@ from .models import Supplier, Item
 class SupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
-        fields = ['supplier_name', 'supplier_address', 'contact_person', 'contact_number']
+        fields = ['supplier_name', 'address', 'contact_person', 'contact_number']
         
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['item_name', 'item_amount', 'unit_mesaurement']
+        fields = ['item_name', 'amount', 'unit_measurement']
